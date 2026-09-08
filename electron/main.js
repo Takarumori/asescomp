4b300a4048cc3f5aaf9f27aaeae7856ad8bf1f7b
Takarumori <cod123sanjim@gmail.com>
2026-09-08 10:25:49 -0600

    cambios de bueno app

---
+++ electron/main.js
@@ -0,0 +1,52 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  let indexPath;
+
+  if (app.isPackaged) {
+    indexPath = path.join(
+      app.getAppPath(),
+      'dist',
+      'asescomp-app',
+      'browser',
+      'index.html'
+    );
+  } else {
+    indexPath = path.join(
+      __dirname,
+      '..',
+      'dist',
+      'asescomp-app',
+      'browser',
+      'index.html'
+    );
+  }
+
+  win.loadFile(indexPath);
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
